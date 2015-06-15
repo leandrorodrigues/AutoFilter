9789b4d382c565b54662ff3c8a9ccd3577b59682
Leandro <leandro@ti-rede-leandro>
2015-06-15 16:19:51 -0300

    Mais desenvolvimento...

--- src/Controller/Component/AutoFilterComponent.php
+++ src/Controller/Component/AutoFilterComponent.php
@@ -2,9 +2,10 @@
 namespace AutoFilter\Controller\Component;
 
 use Cake\Controller\Component;
+use Cake\Controller\Controller;
 use Cake\ORM\Exception\MissingTableClassException;
 use Cake\ORM\Table;
-use Cake\Controller\Component\PaginatorComponent;
+use TermEvaluation\Evaluator;
 /**
  * Componente auxiliar de busca.
  * Tem como principal objetivo auxiliar a montagem de conditions com base em termos buscados e model a ser lido
@@ -16,38 +17,45 @@ class AutoFilterComponent extends Component {
  * @var array Opções padrão
  */
 	private $_options = array();
+
+/**
+ * @var Controller Controller que fez a chamada
+ */
 	private $_controller;
 
 	public function initialize(array $config) {
-		$_controller = $this->_registry->getController();
+		$this->_controller = $this->_registry->getController();
 		$this->_options = array_merge($this->_options, $config);
 	}
 
 /**
  * Faz uma busca em todos os campos disponíveis com base no termo a ser buscado
  *
- * @param string $termo termo a ser buscado
+ * @param string $term termo a ser buscado
  * @param Table &$table Table que irá pesquisar
  * @param array $settings Configurações adicionais (conditions adicionais, joins, etc)
  * @throws MissingTableClassException
  * @return array conditions relacionados a busca
  */
-	public function buscaGeral($termo, Table &$table = null, $settings = array()) {
+	public function generateWhere($term, Table &$table = null, $settings = array()) {
 		if ($table == null) {
-			$model = $this->__Controller->{$this->__Controller->modelClass};
+			$modelClass = $this->_controller->modelClass;
+			list(, $tableName) = pluginSplit($modelClass);
+
+			$table = $this->_controller->{$tableName};
 		}
-		if (!is_object($model)) {
-			throw new MissingModelException($model);
+		if (!is_object($table)) {
+			throw new MissingTableClassException($table);
 		}
 
 		//array que acumula os fields de busca
 		$fields = array();
 
 		//adiciona primeiro os campos do próprio model
-		$fields[$model->name] = $model->schema();
+		$fields[$table->alias()] = $table->schema()->columns();
 
 		//percorrer os joins quando houver
-		if (isset($settings['joins'])) {
+		/*if (isset($settings['joins'])) {
 			foreach ($settings['joins'] as $join) {
 				//nome da classe conforme o nome da tabela do join
 				$modelName = Inflector::classify($join['table']);
@@ -60,11 +68,11 @@ class AutoFilterComponent extends Component {
 					}
 				}
 			}
-		}
+		}*/
 
 		//percorre os belongsTo atras de mais campos
 		//em caso de recursive -1 o cake não faz join automático
-		if ($model->recursive > -1) {
+		/*if ($model->recursive > -1) {
 			foreach ($model->belongsTo as $associationName => $association) {
 				$modelAssoc = $model->$associationName;
 
@@ -75,75 +83,60 @@ class AutoFilterComponent extends Component {
 				}
 
 			}
-		}
-		$conditions = $this->__montarConditions($fields, $termo);
-
-		return $conditions;
-	}
-
-/**
- * Faz a busca do termo e modifica os conditions do paginator
- *
- * @param string $termo termo a ser pesquisado
- * @param Table &$table Table que irá pesquisar
- * @param PaginatorComponent &$paginator Componente de paginação
- * @return void
- */
-	public function buscaGeralPaginada($termo, Table &$table = null, PaginatorComponent &$paginator = null) {
-		if ($paginator == null) {
-			$paginator = $this->_controller->Paginator;
-		}
-
-		/*$conditions = $this->buscaGeral($termo, $table, $paginator->settings);
-
-		if (isset($paginator->settings['conditions']) && is_array($paginator->settings['conditions'])) {
-			$paginator->settings['conditions'] += $conditions;
-		} else {
-			$paginator->settings['conditions'] = $conditions;
 		}*/
-	}
 
-/**
- * Monta um array de conditions baseado nos fields fornecidos e no termo buscado
- *
- * @param array $fields array com todos os campos a ser analisados
- * @param string $termo termo buscado
- *
- * @return array conditions relativos aos fields e termos passados
- *//*
-	private function __montarConditions($fields, $termo) {
-		$conditions = array();
+		$where = array();
 
-		foreach ($fields as $model => $modelFields) {
-			foreach ($modelFields as $fieldName => $properties) {
-				//analisando o contexto do termo buscado
-				//em caso de data formato br
-				if (preg_match('/^[0-9]{2}\/[0-9]{2}\/[0-9]{2,4}$/', $termo)) {
-					if ($properties['type'] == 'date' || $properties['type'] == 'datetime') {
-						list($d, $m, $y) = explode('/', $termo);
-						$y = str_pad($y, 4, '19', STR_PAD_LEFT);
-						$conditions["{$model}.{$fieldName}"] = "{$y}-{$m}-{$d}";
-					}
-				} elseif (preg_match('/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/', $termo)) { //em caso de data com -
-					if ($properties['type'] == 'date' || $properties['type'] == 'datetime') {
-						$conditions["{$model}.{$fieldName}"] = "{$termo}";
-					}
-				} elseif (preg_match('/^[\-\+]?[0-9]*([\.,][0-9]+)?$/', $termo)) { //em caso de número
-					$termo = str_replace(',', '.', $termo);
-					if ($properties['type'] == 'integer') {
-						$termoBusca = (string)round($termo);
-						$conditions["{$model}.{$fieldName}"] = "{$termoBusca}";
-					} elseif ($properties['type'] == 'decimal' || $properties['type'] == 'float') {
-						$conditions["{$model}.{$fieldName}"] = "{$termo}";
-					}
-				} else { //em caso de qualquer termo
-					if ($properties['type'] == 'string') {
-						$conditions[] = "{$model}.{$fieldName} LIKE '%{$termo}%'";
-					}
+		//analizando tipo do termo antes de percorrer os campos.
+		$evaluator = new Evaluator();
+		$evaluatedTerm = $evaluator->evaluate($term);
+
+		foreach ($fields as $tableName => $tableFields) {
+			foreach ($tableFields as $fieldName) {
+				$fieldType = $table->schema()->columnType($fieldName);
+				$whereField = $this->_makeWhere($evaluatedTerm, $tableName . '.' . $fieldName, $fieldType);
+				if($whereField) {
+					$where = array_merge($whereField, $where);
 				}
 			}
 		}
 
-		return array('or' => $conditions);
-	}*/
+		return ['OR' => $where];
+	}
+
+	/**
+	 * Monta um where a partir do termo e do campo passado de acordo com as combinações de tipo.
+	 *
+	 * @param $term string termo a ser pesquisado
+	 * @param $fieldName string campo a ser pesquisado
+	 * @param $fieldType string tipo do campo
+	 *
+	 * @return array parâmetros where montados
+	 */
+	private function _makeWhere($term, $fieldName, $fieldType) {
+		$termType = is_object($term)? get_class($term): gettype($term);
+		$where = [];
+
+		if (($fieldType == 'date' || $fieldType == 'datetime') && $termType == 'DateTime') {
+			$where[] = [$fieldName => $term->format('Y-m-d') ];
+		}
+		elseif ($fieldType == 'integer' && $termType == 'integer') {
+			$where[] = [$fieldName => (string)$term];
+		}
+		elseif (($fieldType == 'decimal' || $fieldType == 'float') && $termType == 'double') {
+			$where[] = [$fieldName => (string)$term];
+		}
+		//em caso de qualquer termo
+		else {
+			if ($fieldType == 'string' && $termType == 'string') {
+				$where[] = "{$fieldName} LIKE '%{$term}%'";
+			}
+		}
+
+		if(empty($where)) {
+			return false;
+		}
+		
+		return $where;
+	}
 }
